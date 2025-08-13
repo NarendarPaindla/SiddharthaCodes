22436b768d0bec9a0a7b7a27166682f81e7f45e8
NarendarTrainer <narendartrainer@gmail.com>
2025-08-13 13:46:08 +0530

    first program

---
+++ JavaPrograms/L1.java
@@ -0,0 +1,17 @@
+
+
+public class L1
+{
+	public static void main(String[] args) {
+	byte b=10;
+	short s=300;
+	int i=1000000000;
+	long l=100000000L;
+	float f=3.14f;
+	boolean ok=true;
+	String name="java";
+	char c='A';
+	System.out.println("My name is: "+name+" float value: "+f);
+	System.out.println("integer value: "+i);
+	}
+}
