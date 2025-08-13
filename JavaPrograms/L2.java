561f8afada53e72eea8a339557fb58fc7117898a
NarendarTrainer <narendartrainer@gmail.com>
2025-08-13 14:06:43 +0530

    l2

---
+++ JavaPrograms/L2.java
@@ -0,0 +1,11 @@
+public class L2
+{
+	public static void main(String[] args) {
+    int a =5,b=6;
+    System.out.println(a+b);
+    System.out.println(a/b);
+    System.out.println(a%2==1);
+    a=a+5;
+    System.out.println(a>10 && b<5);
+	}
+}
